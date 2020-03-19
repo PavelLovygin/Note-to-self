9b790e222184e419c2d5f1ad514b6aa380341d7c
PavelLovygin <pavel.lovygin.00@mail.ru>
2020-03-19 12:42:53 +0500

    SettingActivity

--- app/src/main/java/com/example/notetoself/MainActivity.java
+++ app/src/main/java/com/example/notetoself/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.notetoself;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,6 +13,7 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     NoteAdapter adapter;
     ArrayList<Note> notes = new ArrayList<>();
+    private JSONSerializer serializer;
+    private ArrayList<Note> noteList;
+
+    private boolean showDividers;
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +49,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        serializer = new JSONSerializer("NoteToSelf.json", getApplicationContext());
+        try {
+            notes = serializer.load();
+        } catch (Exception e) {
+            notes = new ArrayList<>();
+            Log.e("Error loading notes: ", "", e);
+        }
+
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         adapter = new NoteAdapter(this, notes);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
 
-        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
         recyclerView.setAdapter(adapter);
+        if (showDividers) {
+            recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        } else {
+            if (recyclerView.getItemDecorationCount() > 0) {
+                recyclerView.removeItemDecorationAt(0);
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        prefs = getSharedPreferences("Note to self", MODE_PRIVATE);
+        showDividers = prefs.getBoolean("dividers", true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        saveNotes();
     }
 
     @Override
@@ -67,6 +104,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingActivity.class);
+            startActivity(intent);
             return true;
         }
 
@@ -83,5 +122,13 @@ public class MainActivity extends AppCompatActivity {
         DialogShowNote dialog = new DialogShowNote();
         dialog.sendNoteSelected(notes.get(index));
         dialog.show(getSupportFragmentManager(), "");
+    }
+
+    public void saveNotes() {
+        try {
+            serializer.save(notes);
+        } catch (Exception e) {
+            Log.e("Error saving Notes", "", e);
+        }
     }
 }
